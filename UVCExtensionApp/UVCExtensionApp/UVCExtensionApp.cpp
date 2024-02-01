1cf4223bd6b5046803a412e33ce16c38d073c69b
YixingShen <kkman3651878@hotmail.com>
2024-02-01 08:30:45 +0800

    update

--- UVCExtensionApp/UVCExtensionApp/UVCExtensionApp.cpp
+++ UVCExtensionApp/UVCExtensionApp/UVCExtensionApp.cpp
@@ -7,14 +7,39 @@
 #include <mfreadwrite.h>
 #include <vector>
 #include <string>
+
+//#include <ks.h>
+//#include <ksproxy.h>
+//#include <vidcap.h>
+//#include "UVCExtensionApp.h"
+//#include <ksmedia.h>
+//#include "UVCExtensionApp.h"
+
+#include <initguid.h>
+#include <uuids.h>
+
 #include <ks.h>
+#include <ksmedia.h>
 #include <ksproxy.h>
-#include <vidcap.h>
 #include "UVCExtensionApp.h"
+
+//#include <vidcap.h>
+//#include <ks.h>
+//#include <ksproxy.h>
+//#include <ksmedia.h>
+//#include <initguid.h>
+//#include <uuids.h>
+//#include "UVCExtensionApp.h"
+
 
 // GUID of the extension unit, {ACB6890C-A3B3-4060-8B9A-DF34EEF39A2E}
 static const GUID xuGuidAN75779 =
 { 0xacb6890c, 0xa3b3, 0x4060,{ 0x8b, 0x9a, 0xdf, 0x34, 0xee, 0xf3, 0x9a, 0x2e } };
+
+//
+// GUID of the extension unit, {1BD7F382-0EAE-46AB-8CB4-9E00E1EC643F}
+static const GUID xuGuidTinyUSB =
+{ 0x1bd7f382, 0x0eae, 0x46ab,{ 0x8c, 0xb4, 0x9e, 0x00, 0xe1, 0xec, 0x64, 0x3f } };
 
 //Media foundation and DSHOW specific structures, class and variables
 IMFMediaSource *pVideoSource = NULL;
@@ -25,6 +50,11 @@ IMFSourceReader *pVideoReader = NULL;
 //Other variables
 UINT32 noOfVideoDevices = 0;
 WCHAR *szFriendlyName = NULL;
+//#define CAMERA_NAME "TinyUSB UVC1"
+#define CAMERA_NAME "TinyUSB UVC"
+DWORD ExtensionNode;
+
+#define SAFE_RELEASE(x) { if (x) x->Release(); x = NULL; }
 
 int main()
 {
@@ -33,7 +63,7 @@ int main()
 	UINT32 selectedVal = 0xFFFFFFFF;
 	ULONG flags, readCount;
 	size_t returnValue;
-	BYTE an75779FwVer[5] = { 2, 2, 12, 20, 18 }; //Write some random value
+	BYTE an75779FwVer[64] = { 2, 2, 12, 20, 18 }; //Write some random value
 
 	//Get all video devices
 	GetVideoDevices();
@@ -47,14 +77,16 @@ int main()
 		printf("%d: %s\n", i, videoDevName[i]);
 
 		//Store the App note firmware (Whose name is *FX3*) device index  
-		if (!(strcmp(videoDevName[i], "FX3")))
+		//if (!(strcmp(videoDevName[i], "FX3")))
+        if (!(strcmp(videoDevName[i], CAMERA_NAME)))
 			selectedVal = i;
 	}
 
 	//Specific to UVC extension unit in AN75779 appnote firmware
 	if (selectedVal != 0xFFFFFFFF)
 	{
-		printf("\nFound \"FX3\" device\n");
+		//printf("\nFound \"FX3\" device\n");
+        printf("\nFound %s device\n", CAMERA_NAME);
 
 		//Initialize the selected device
 		InitVideoDevice(selectedVal);
@@ -71,7 +103,50 @@ int main()
 
 		printf("\nTrying to invoke UVC extension unit...\n");
 
-		if (!SetGetExtensionUnit(xuGuidAN75779, 2, 1, flags, (void*)an75779FwVer, 5, &readCount))
+#if 1 //Find Extension Node
+        DWORD numNodes = 0;
+        GUID guidNodeType;
+        ULONG bytesReturned = 0;
+        IKsTopologyInfo *pKsTopologyInfo;
+        KSP_NODE kspNode;
+        IKsControl *ks_control;
+
+        HRESULT hr = pVideoSource->QueryInterface(__uuidof(IKsTopologyInfo), (void **)&pKsTopologyInfo);
+
+        pKsTopologyInfo->get_NumNodes(&numNodes);
+
+        for (DWORD ulNode = 0; ulNode< numNodes; ulNode++)
+        {
+            pKsTopologyInfo->get_NodeType(ulNode, &guidNodeType);
+
+            if (IsEqualGUID(guidNodeType, KSNODETYPE_DEV_SPECIFIC))
+            {
+                ExtensionNode= ulNode;
+
+                if ((hr = pKsTopologyInfo->CreateNodeInstance(ulNode, IID_IKsControl, (void **)&ks_control)) == S_OK)
+                {
+                    kspNode.Property.Set = xuGuidTinyUSB;
+                    kspNode.Property.Id = KSPROPERTY_EXTENSION_UNIT_INFO; //0x00
+                    kspNode.Property.Flags = KSPROPERTY_TYPE_SETSUPPORT | KSPROPERTY_TYPE_TOPOLOGY;
+                    kspNode.NodeId = ulNode;
+                    kspNode.Reserved = 0;
+
+                    if ((hr = ((IKsControl *)ks_control)->KsProperty((PKSPROPERTY)&kspNode, sizeof(kspNode), NULL, 0, &bytesReturned)) != S_OK)
+                    {
+                        ks_control->Release();
+                        continue;
+                    }
+
+                    SAFE_RELEASE(ks_control);
+                    break;
+                }
+            }
+        }
+
+        SAFE_RELEASE(pKsTopologyInfo);
+#endif
+		//if (!SetGetExtensionUnit(xuGuidAN75779, 2, 1, flags, (void*)an75779FwVer, 5, &readCount))
+        if (!SetGetExtensionUnit(xuGuidTinyUSB, ExtensionNode, 1, flags, (void*)an75779FwVer, 64, &readCount))
 		{
 			printf("Found UVC extension unit\n");
 
@@ -182,14 +257,13 @@ HRESULT SetGetExtensionUnit(GUID xuGuid, DWORD dwExtensionNode, ULONG xuProperty
 
 	hr = unKnown->QueryInterface(__uuidof(IKsControl), (void **)&ks_control);
 	CHECK_HR_RESULT(hr, "ks_topology_info->QueryInterface(...)");
+    kspNode.Property.Set = xuGuid;             // XU GUID
+    kspNode.NodeId = (ULONG)dwExtensionNode;   // XU Node ID
+    kspNode.Property.Id = xuPropertyId;        // XU control ID
+    kspNode.Property.Flags = flags;            // Set/Get request
 
-	kspNode.Property.Set = xuGuid;              // XU GUID
-	kspNode.NodeId = (ULONG)dwExtensionNode;   // XU Node ID
-	kspNode.Property.Id = xuPropertyId;         // XU control ID
-	kspNode.Property.Flags = flags;             // Set/Get request
-
-	hr = ks_control->KsProperty((PKSPROPERTY)&kspNode, sizeof(kspNode), (PVOID)data, len, readCount);
-	CHECK_HR_RESULT(hr, "ks_control->KsProperty(...)");
+    hr = ks_control->KsProperty((PKSPROPERTY)&kspNode, sizeof(kspNode), (PVOID)data, len, readCount);
+    CHECK_HR_RESULT(hr, "ks_control->KsProperty(...)");
 
 done:
 	SafeRelease(&ks_control);
